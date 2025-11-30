857d6f90be86fbc03460d4fb81bf559b20387dfe
MaelysCC <maelys.de-crouy-chanel@efrei.net>
2025-11-30 19:49:16 +0530

    Update index.js

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -21,7 +21,7 @@ const routes = [
   { path: "/form-recruiter", component: FormRecruiter, meta: { requiresAuth: true } },
   { path: '/profil-c', component: ProfilCandidat, meta: { requiresAuth: true, role: 'candidate' } },
   { path: "/profil-r", component: ProfilRecruiter, meta: { requiresAuth: true, role: 'recruiter' } },
-  { path: '/addjob', name: 'AddJob', component: AddJob },
+  { path: '/addjob', name: 'AddJob', component: AddJob }
 
 ]
 
