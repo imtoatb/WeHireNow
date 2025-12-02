71b8a61f2db5eff3fc43e88712941d61a31cd8af
MaelysCC <maelys.de-crouy-chanel@efrei.net>
2025-12-02 12:03:52 +0530

    v9

--- backend/server.js
+++ backend/server.js
@@ -32,8 +32,9 @@ app.use(cors({
 }));
 
 // Middleware JSON
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// Accepter des JSON + formulaires plus gros (images base64)
+app.use(express.json({ limit: '2mb' }));
+app.use(express.urlencoded({ extended: true, limit: '2mb' }));
 
 
 // Session setup
